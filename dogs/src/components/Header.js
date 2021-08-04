ee30a85a248861e83052eb783e5f5c3b7544aca2
Monika <monika.silvia.s@hotmail.ca>
2021-08-04 15:15:41 -0700

    Started Creating componenets
    Created a basic layout and started to test out how to create components.

---
+++ dogs/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header className="App-header">
+                <div>TEST</div>
+            </header>
+        );
+    }
+}
+
+export default Header;
