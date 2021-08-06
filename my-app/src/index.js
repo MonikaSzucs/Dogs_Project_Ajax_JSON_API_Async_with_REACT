7145bb198db9700d943f3b01eb941000ca579c5c
Monika <monika.silvia.s@hotmail.ca>
2021-08-06 13:14:03 -0700

    Adding CSS properties

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//the '.' means that it is in the same folder
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 function DogList(){
   return (
-    <section>
-      <Book/>
+    <section className="booklist">
+      <Book />
       <Book/>
       <Book/>
     </section>
@@ -16,7 +17,7 @@ function DogList(){
 
 const Book = () => {
   return (
-    <article>
+    <article className="book">
         <Image/>
         <Title/>
         <Author/>
@@ -25,8 +26,8 @@ const Book = () => {
 }
 const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/51o4b5AdNLL._AC_SX184_.jpg" alt=""/>
 
-const Title = () => <h4>TITLE HERE</h4>
+const Title = () => <h1>TITLE HERE</h1>
 
-const Author = () => <p>Author</p>
+const Author = () => <h4>Author</h4>
 
 ReactDOM.render(<DogList/>, document.getElementById('root'));
