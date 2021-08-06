8127f320840fcc9c19d9dab6faede146b3f81254
Monika <monika.silvia.s@hotmail.ca>
2021-08-06 12:37:48 -0700

    Nested components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Greeting(){
+  return (
+    <div>
+      <Person/>
+      <p>This is my message.</p>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Person = () => <h2>John Doe</h2>
+
+ReactDOM.render(<Greeting/>, document.getElementById('root'));
